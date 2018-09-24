e2f5331c116deb9051fb0c2ea1444558fac347e0
paulo <pauloh16.pdr@gmail.com>
2018-09-24 09:11:22 -0300

    Commitar exemplos de while

---
+++ 301/js.js
@@ -0,0 +1,19 @@
+/*let numero = Number(prompt("Até que número você quer contar?"));
+let contador = 0;
+
+
+while(contador <= numero){
+    alert(contador);
+
+    contador = contador + 1;
+}*/
+
+/*let tabuada = Number(prompt("Digite um número para ver a tabuada"));
+let limite = Number(prompt("Até quando vai a tabuada para você?"));
+let numero = 0;
+
+while(numero <= limite){
+   console.log(`${tabuada} X ${numero} = ${tabuada*numero}`);
+
+    numero++;
+}*/
