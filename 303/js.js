61f2402d64f649ea21358e50f4080a262eb037af
Paulo <pauloh16.pdr@gmail.com>
2018-09-26 08:45:05 -0300

    Commitar explicações de For

---
+++ 303/js.js
@@ -0,0 +1,24 @@
+let limite = 10;
+let i= 0;
+
+while(i < limite){
+    codigo
+    codigo
+    codigo
+    codigo
+
+    i++
+}
+
+for(let i = 0; i < limite; i++){
+    codigo
+    codigo
+    codigo
+    codigo
+}
+
+let continuar = true;
+
+while(continuar){
+    continuar = confirm("Quer continuar?")
+}
