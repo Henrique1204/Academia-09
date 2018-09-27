46457367e3d189c328f84fac145286ffdd4a3177
Paulo <pauloh16.pdr@gmail.com>
2018-09-27 12:04:40 -0300

    Commitar mais coisas da aula 304

--- 304/js.js
+++ 304/js.js
@@ -8,4 +8,13 @@ variavel.style("mostra todo o css do elemento")
 para acessar o conteúdo da tag é variavel.innerHTML
 para mudar é só colocar varivavel.innerHTML = "mudança que você quer"
 variavel.classList.replace('classe atual', 'class que você quer no lugar da atual') serve para trocar
-creatorElement("cria um novo elemento")
+creatorElement("cria um novo elemento"
+*/
+
+const inputCor = document.querySelector("#cor");
+
+function trocarCorDoInput(){
+    inputCor.style.backgroundColor = inputCor.value;
+}
+
+inputCor.onkeyup = trocarCorDoInput;
